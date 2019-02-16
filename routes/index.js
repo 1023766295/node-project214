0d0099f3cec4d8b65aa03d4e11b312c9bcb61460
1023766295 <1023766295@qq.com>
2019-02-16 15:49:00 +0800

    今天写了2小时css,布局勉强能看

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,19 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+/* GET users page. */
+router.get('/users.html', function(req, res) {
+  res.render('users', { title: 'Express' });
+});
+
+/* GET users page. */
+router.get('/brand.html', function(req, res) {
+  res.render('brand', { title: 'Express' });
+});
+
+/* GET users page. */
+router.get('/phone.html', function(req, res) {
+  res.render('phone', { title: 'Express' });
+});
+
 module.exports = router;
