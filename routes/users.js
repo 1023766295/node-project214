0ea197a8dae15b50a2f1aad59028818e1cfe6554
1023766295 <1023766295@qq.com>
2019-02-21 14:32:54 +0800

    前面的太low

--- routes/users.js
+++ routes/users.js
@@ -1,32 +1,92 @@
 var express = require('express');
 var MongoClient=require('mongodb').MongoClient;
+var ObjectId=require('mongodb').ObjectId;
 var router = express.Router();
 var async = require('async');
 
 const url="mongodb://127.0.0.1:27017";
 
-/* GET users listing. */
+/* GET users listing. 分页效果*/
 router.get('/', function(req, res, next) {
+  var page=parseInt(req.query.page) || 1;//总页数
+  var pageSize=parseInt(req.query.pageSize) || 3;//每页条数
+  var totalSize=0;//总条数
+  var totalPage=0;
+
   MongoClient.connect(url,{ useNewUrlParser: true },function(err,client){
     var db=client.db('project214');
-    db.collection('user').find().toArray(function(err,data){
-      if(err){
-        console.log('查询用户数据失败',err);
-        // 有错误，渲染 error,ejs
-        res.render('error',{
-          message:'查询失败',
-          error:err
-        });
-        return;
-      }else{
-        console.log(data);
-        res.render('users', {
-          list: data
+    if(err){
+      console.log('连接失败',err);
+      res.render('error',{
+        message:'连接失败',
+        error:err
+      })
+      return;
+    }
+    var db=client.db('project214');
+    
+    async.series([
+      function(cb){
+        db.collection('user').find().count(function(err,num){
+          if(err){
+            cb(err);
+          }else{
+            totalSize =num;
+            totalPage=Math.ceil(totalSize/pageSize);
+            cb(null);
+          }
+        })
+      },function(cb){
+        // db.collection('user').find().limit(5).skip(0)
+        // db.collection('user').find().limit(5).skip(5)
+        // db.collection('user').find().limit(5).skip(10)
+        db.collection('user').find().limit(pageSize).skip(pageSize * page - pageSize).toArray(function(err, data){
+          if(err){
+            cb(err);
+          }else{
+            cb(null,data);
+          }
         });
       }
-    });
-    client.close();
-  })  
+    ],function(err,results){
+      if(err){
+        res.render('error',{
+          message:'查找数据失败',
+          error:err
+        })
+      }else{
+        res.render('users',{
+          list:results[1],
+          totalPage:totalPage,
+          pageSize:pageSize,
+          currentPage:page
+        });
+        client.close();
+      }
+    })
+  })
+
+  // 少量数据的情况，多了要分页，如上====================
+  // MongoClient.connect(url,{ useNewUrlParser: true },function(err,client){
+  //   var db=client.db('project214');
+  //   db.collection('user').find().toArray(function(err,data){
+  //     if(err){
+  //       console.log('查询用户数据失败',err);
+  //       // 有错误，渲染 error,ejs
+  //       res.render('error',{
+  //         message:'查询失败',
+  //         error:err
+  //       });
+  //       return;
+  //     }else{
+  //       console.log(data);
+  //       res.render('users', {
+  //         list: data
+  //       });
+  //     }
+  //   });
+  //   client.close();
+  // })  
 });
 
 // 登录
@@ -113,9 +173,9 @@ router.post('/login',function(req,res){
         })
         res.redirect('/');
       }
+      client.close();
     })
   })   
-
   // res.send('b');
 })
 
@@ -205,6 +265,41 @@ MongoClient.connect(url,{useNewUrlParser:true},function(err,client){
     }
     client.close();
   })
+})
+
+// 删除数据库的一条数据
+router.get('/delete',function(req,res){
+  // 获取前端传递过来的参数
+  //  console.log(req.query);
+  var id=req.query.id;
+  // 连接数据库做验证
+  MongoClient.connect(url,{useNewUrlParser:true},function(err,client){
+    if(err){
+      console.log("连接失败",err);
+      res.render('error',{
+        message:'查询失败',
+        error:err
+      })
+      return;
+    }
+
+    var db=client.db('project214');
+
+    db.collection('user').deleteOne({
+      _id: ObjectId(id)
+    },function(err,data){
+      console.log(data);
+      if(err){
+        res.render('error',{
+          message:'删除失败',
+          error:err
+        })
+      }else{
+        res.redirect('/users');
+      }
+      client.close();
+    })
+  })   
 })
 
   // 连接数据库做验证
